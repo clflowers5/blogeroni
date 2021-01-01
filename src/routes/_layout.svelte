aeb12abf8e1d56d5404e051145b625a23d10167e
Chris Flowers <clflowers5@gmail.com>
2021-01-01 07:59:04 -0700

    Random style stuff. Not much changed.

--- src/routes/_layout.svelte
+++ src/routes/_layout.svelte
@@ -1,22 +1,22 @@
 <script>
 	import Nav from '../components/Nav.svelte';
-
 	export let segment;
 </script>
 
-<style>
+<style type="text/scss">
+	@import "../styles/base.scss";
 	main {
 		position: relative;
 		max-width: 56em;
-		background-color: white;
 		padding: 2em;
 		margin: 0 auto;
 		box-sizing: border-box;
 	}
 </style>
 
-<Nav {segment}/>
-
-<main>
-	<slot></slot>
-</main>
+<div class="root">
+	<Nav {segment}/>
+	<main>
+		<slot></slot>
+	</main>
+</div>
