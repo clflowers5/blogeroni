520f4e2efd5a858549f03871b75b9a65eb6d468b
Chris Flowers <clflowers5@gmail.com>
2021-01-18 08:04:49 -0700

    tech(css): testing css optimization

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,11 @@
+const cssnano = require('cssnano');
 const tailwindcss = require("tailwindcss");
+
+const production = process.env.NODE_ENV === 'production';
 
 module.exports = {
   plugins: [
     tailwindcss("./tailwind.js"),
+    production ? cssnano({ preset: 'default' }) : null,
   ]
 };
